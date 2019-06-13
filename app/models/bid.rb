17eff08c97e863822547dd59cd93381e65488f08
Ruslanwww <oruslano51@gmail.com>
2019-06-13 11:37:14 +0300

    Added annotate

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: bids
+#
+#  id                :integer          not null, primary key
+#  bid_creation_time :datetime
+#  proposed_price    :decimal(, )
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  lot_id            :integer
+#  user_id           :integer
+#
+# Indexes
+#
+#  index_bids_on_lot_id   (lot_id)
+#  index_bids_on_user_id  (user_id)
+#
+
 class Bid < ApplicationRecord
   belongs_to :user
   belongs_to :lot
