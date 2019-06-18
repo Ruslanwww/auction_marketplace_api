6269f8cdd5db7dc30d6fb5b9ca45504f66773e54
Ruslanwww <oruslano51@gmail.com>
2019-06-18 14:13:26 +0300

    Added tests for associations

--- spec/models/lot_spec.rb
+++ spec/models/lot_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Lot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { should belong_to(:user) }
+    it { should have_many(:bids).dependent(:destroy) }
+    it { should have_one(:order).dependent(:destroy) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  describe "Associations" do
+    it { should have_many(:lots).dependent(:destroy) }
+    it { should have_many(:orders).dependent(:destroy) }
+    it { should have_many(:bids).dependent(:destroy) }
+  end
+
   it "should create a user" do
     expect { create :user }.to change { User.count }.by(1)
   end
